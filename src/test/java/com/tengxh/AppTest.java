356e413313f108fcb2770a1aef5da863117e1eda
tengxh <330786390@qq.com>
2018-08-04 10:00:40 +0800

    增加：文件、xml

--- src/test/java/com/tengxh/AppTest.java
+++ src/test/java/com/tengxh/AppTest.java
@@ -1,20 +1,23 @@
 package com.tengxh;
 
-import static org.junit.Assert.assertTrue;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+    protected final Log log = LogFactory.getLog(getClass());
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        log.info("this is test");
+        assertTrue(true);
     }
 }
