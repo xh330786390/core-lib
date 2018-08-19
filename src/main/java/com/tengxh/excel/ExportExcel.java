a97937fe7095163292a8e46a1f993bc49e741817
tengxh <330786390@qq.com>
2018-08-19 12:28:25 +0800

    添加 poi 操作Excel

---
+++ src/main/java/com/tengxh/excel/ExportExcel.java
@@ -0,0 +1,143 @@
+package com.tengxh.excel;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName: ExportExcel.java
+ * @Description: java类作用描述
+ * @Author: tengxh
+ * @CreateDate: 2018/8/19
+ * @Version: 1.0
+ */
+public class ExportExcel {
+    //读取Xlsx
+    public static Map<Integer, List<String[]>> readXlsx(String fileName) {
+
+        Map<Integer, List<String[]>> map = new HashMap<Integer, List<String[]>>();
+        try {
+            InputStream is = new FileInputStream(fileName);
+            XSSFWorkbook workbook = new XSSFWorkbook(is);
+
+            System.out.println("sheet count: " + workbook.getNumberOfSheets());
+
+            // 循环工作表Sheet
+            for (int numSheet = 0; numSheet < workbook.getNumberOfSheets(); numSheet++) {
+                XSSFSheet xssfSheet = workbook.getSheetAt(numSheet);
+                if (xssfSheet == null) {
+                    continue;
+                }
+                List<String[]> list = new ArrayList<String[]>();
+
+                for (int row = 0; row <= xssfSheet.getLastRowNum(); row++) {
+                    XSSFRow xssfRow = xssfSheet.getRow(row);
+                    if (xssfRow == null) {
+                        continue;
+                    }
+                    String[] singleRow = new String[xssfRow.getLastCellNum()];
+                    for (int column = 0; column < xssfRow.getLastCellNum(); column++) {
+                        Cell cell = xssfRow.getCell(column, Row.CREATE_NULL_AS_BLANK);
+                        switch (cell.getCellType()) {
+                            case Cell.CELL_TYPE_BLANK:
+                                singleRow[column] = "";
+                                break;
+                            case Cell.CELL_TYPE_BOOLEAN:
+                                singleRow[column] = Boolean.toString(cell.getBooleanCellValue());
+                                break;
+                            case Cell.CELL_TYPE_ERROR:
+                                singleRow[column] = "";
+                                break;
+                            case Cell.CELL_TYPE_FORMULA:
+                                cell.setCellType(Cell.CELL_TYPE_STRING);
+                                singleRow[column] = cell.getStringCellValue();
+                                if (singleRow[column] != null) {
+                                    singleRow[column] = singleRow[column].replaceAll("#N/A", "").trim();
+                                }
+                                break;
+                            case Cell.CELL_TYPE_NUMERIC:
+                                if (DateUtil.isCellDateFormatted(cell)) {
+                                    singleRow[column] = String.valueOf(cell.getDateCellValue());
+                                } else {
+                                    cell.setCellType(Cell.CELL_TYPE_STRING);
+                                    String temp = cell.getStringCellValue();
+                                    // 判断是否包含小数点，如果不含小数点，则以字符串读取，如果含小数点，则转换为Double类型的字符串
+                                    if (temp.indexOf(".") > -1) {
+                                        singleRow[column] = String.valueOf(new Double(temp)).trim();
+                                    } else {
+                                        singleRow[column] = temp.trim();
+                                    }
+                                }
+
+                                break;
+                            case Cell.CELL_TYPE_STRING:
+                                singleRow[column] = cell.getStringCellValue().trim();
+                                break;
+                            default:
+                                singleRow[column] = "";
+                                break;
+                        }
+                    }
+                    list.add(singleRow);
+                }
+                map.put(numSheet, list);
+            }
+        } catch (FileNotFoundException e) {
+            // TODO 自动生成的 catch 块
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO 自动生成的 catch 块
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    //写入Xlsx
+    public static void writeXlsx(String fileName, Map<Integer, List<String[]>> map) {
+        try {
+            XSSFWorkbook wb = new XSSFWorkbook();
+            for (int sheetnum = 0; sheetnum < map.size(); sheetnum++) {
+                XSSFSheet sheet = wb.createSheet("" + sheetnum);
+                List<String[]> list = map.get(sheetnum);
+                for (int i = 0; i < list.size(); i++) {
+                    XSSFRow row = sheet.createRow(i);
+                    String[] str = list.get(i);
+                    for (int j = 0; j < str.length; j++) {
+                        XSSFCell cell = row.createCell(j);
+                        cell.setCellValue(str[j]);
+                    }
+                }
+            }
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            wb.write(outputStream);
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            // TODO 自动生成的 catch 块
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO 自动生成的 catch 块
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("开始读取Excle数据");
+        Map<Integer, List<String[]>> listMap = readXlsx("C:\\Users\\luck_\\Desktop\\1.xlsx");
+        if (listMap != null) {
+            System.out.println("开始导出Excle数据");
+            writeXlsx("C:\\Users\\luck_\\Desktop\\2.xlsx", listMap);
+        }
+        System.out.println("");
+    }
+}
---
+++ src/main/java/com/tengxh/excel/ReadExcel.java
@@ -0,0 +1,11 @@
+package com.tengxh.excel;
+
+/**
+ * @ClassName: ReadExcel.java
+ * @Description: java类作用描述
+ * @Author: tengxh
+ * @CreateDate: 2018/8/19
+ * @Version: 1.0
+ */
+public class ReadExcel {
+}
